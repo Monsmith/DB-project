32f701f73b808619f5137825b8214423d619af04
Vachiravit <vachiravit.va@gmail.com>
2018-11-08 00:02:02 +0700

    solve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,27 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_11_07_162220) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
-
-  create_table 'chatroom_users', force: :cascade do |t|
-    t.bigint 'chatroom_id'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['chatroom_id'], name: 'index_chatroom_users_on_chatroom_id'
-    t.index ['user_id'], name: 'index_chatroom_users_on_user_id'
-  end
-
-  create_table 'chatrooms', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
-  create_table 'groups', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
+  enable_extension "plpgsql"
 
   create_table "book_marks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -106,23 +84,14 @@ ActiveRecord::Schema.define(version: 2018_11_07_162220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_photos_on_post_id"
-  create_table 'likes', force: :cascade do |t|
-    t.bigint 'post_id'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 
-  create_table 'messages', force: :cascade do |t|
-    t.bigint 'chatroom_id'
-    t.bigint 'user_id'
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['chatroom_id'], name: 'index_messages_on_chatroom_id'
-    t.index ['user_id'], name: 'index_messages_on_user_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -140,12 +109,6 @@ ActiveRecord::Schema.define(version: 2018_11_07_162220) do
     t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  create_table 'photos', force: :cascade do |t|
-    t.string 'image'
-    t.bigint 'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_photos_on_post_id'
   end
 
   add_foreign_key "bookmarks", "posts"
@@ -160,37 +123,4 @@ ActiveRecord::Schema.define(version: 2018_11_07_162220) do
   add_foreign_key "messages", "users"
   add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
-  create_table 'posts', force: :cascade do |t|
-    t.string 'content'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_posts_on_user_id'
-  end
-
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.string 'image'
-    t.text 'bio'
-    t.string 'website'
-    t.string 'roles'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-  end
-
-  add_foreign_key 'chatroom_users', 'chatrooms'
-  add_foreign_key 'chatroom_users', 'users'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'messages', 'chatrooms'
-  add_foreign_key 'messages', 'users'
-  add_foreign_key 'photos', 'posts'
-  add_foreign_key 'posts', 'users'
 end
