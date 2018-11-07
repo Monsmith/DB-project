dc8bb0bc47073586df5ff22193da8454c2495ebe
Vachiravit <vachiravit.va@gmail.com>
2018-11-08 01:55:16 +0700

    test comment

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Admin::BaseController < ApplicationController
-  before_action :ensure_admin!
-  before_action :authenticate_user!
-
-  def ensure_admin!
-    unless current_user.roles == %i[admin user]
-      redirect_to root_path, danger: "You don't belong there"
-    end
-  end
+  # before_action :ensure_admin!
+  # before_action :authenticate_user!
+  #
+  # def ensure_admin!
+  #   unless current_user.roles == %i[admin user]
+  #     redirect_to root_path, danger: "You don't belong there"
+  #   end
+  # end
 end
