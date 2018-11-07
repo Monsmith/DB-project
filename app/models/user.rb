15be99a46015bf74464edcea2045952338935dd2
Vachiravit <vachiravit.va@gmail.com>
2018-11-07 23:49:03 +0700

    has ability to comment and like

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :chatroom_users
   has_many :chatroom_users
   has_many :messages
+  has_many :likes
+  has_many :bookmarks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,11 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: %i[index show create destroy] do
+  resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
+    resources :likes, only: [:create, :destroy], shallow: true
+    resources :comments, only: [:index, :create, :destroy], shallow: true
+    resources :bookmarks, only: [:create, :destroy], shallow: true
   end
 
   get 'group', to: 'chatrooms#index'
